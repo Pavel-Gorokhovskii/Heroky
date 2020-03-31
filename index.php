4ef7047bfa92e30f74cc7fcc945077f5daa2ba95
Pavel-Gorokhovskii <Pavel-Gorokhovskii@yandex.ru>
2020-03-31 21:05:58 +0300

    Update index.php

--- index.php
+++ index.php
@@ -23,7 +23,8 @@ Port: 3306
     $link = mysqli_connect("remotemysql.com", "5HDGpZQpM6", "HxrlVnUEGT")
         or die("Не удалось соединиться с сервером");
 
-
+    mysqli_select_db($link, "guest_book")
+        or die("Не удалось выбрать БД");
 
     // echo 'Hello WORLD!!!';
 
